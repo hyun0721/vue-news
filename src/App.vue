45367fa4a35e034c715174e55d88348d24db410f
donghyun <hyunee721@gmail.com>
2023-07-23 22:09:48 +0900

    1. ToolBar 생성 : router-link 2. 실습파일 생성

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+<template>  
+  <ToolBar></ToolBar>
   <router-view></router-view>
 </template>
 
 <script>
+import ToolBar from '@/components/ToolBar.vue';
 
 export default {
   name: 'App',
+  components: {
+    ToolBar
+  }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body{
+    padding: 0;
+    margin: 0;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
-import HomePage from '@/views/HomePage.vue';
-import NewsView from '@/views/NewsView.vue';
-import AskView  from '@/views/AskView.vue';
-import JobsView from '@/views/JobsView.vue';
+import HomePage from "@/views/HomePage.vue";
+import NewsView from "@/views/NewsView.vue";
+import AskView from "@/views/AskView.vue";
+import JobsView from "@/views/JobsView.vue";
+import ItemView from "@/views/ItemView.vue";
+import UserView from "@/views/UserView.vue"
 
 const routes = [
-    { path: "/", component: HomePage },
+    { path: "/", redirect: '/news' , component: HomePage },
     { path: "/news", component: NewsView },
     { path: "/ask", component: AskView },
     { path: "/jobs", component: JobsView },
+    { path: "/Item", component: ItemView },
+    { path: "/User", component: UserView },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes,
 });
 
-export { router }
+export { router };
