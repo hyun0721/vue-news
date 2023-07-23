b8207e386a830e14d6636f79812be6fc728bd887
donghyun <hyunee721@gmail.com>
2023-07-23 21:35:47 +0900

    1. Vue Router 설치 2. View 페이지 구성 3. router 모듈화

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import HomePage from '@/views/HomePage.vue'
+import HomePage from '@/views/HomePage.vue';
+import NewsView from '@/views/NewsView.vue';
+import AskView  from '@/views/AskView.vue';
+import JobsView from '@/views/JobsView.vue';
 
 const routes = [
     { path: "/", component: HomePage },
+    { path: "/news", component: NewsView },
+    { path: "/ask", component: AskView },
+    { path: "/jobs", component: JobsView },
 ];
 
 const router = createRouter({
---
+++ src/views/NewsView.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        News View
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    
+</style>
