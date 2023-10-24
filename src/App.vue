c7b002e3e71832b8c52a4770e375c089abc5e7fa
donghyun <hyunee721@gmail.com>
2023-10-24 21:41:52 +0900

    [main] Add Router Transition

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>  
   <ToolBar></ToolBar>
-  <router-view></router-view>
+  <transition name="page">
+    <router-view></router-view>
+  </transition>  
 </template>
 
 <script>
 import ToolBar from '@/components/ToolBar.vue';
+// import { Transition } from 'vue';
 
 export default {
   name: 'App',
@@ -16,8 +19,21 @@ export default {
 </script>
 
 <style>
-  body{
-    padding: 0;
-    margin: 0;
-  }
+
+body{
+  padding: 0;
+  margin: 0;
+}
+
+/* Router Transition : {'page'} 라는 명칭에 대한 name에 대한 명명 규칙을 따름 */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+
 </style>
