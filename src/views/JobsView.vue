fa3eb409c4a7385166e89566dc272d3fb14b771d
donghyun <hyunee721@gmail.com>
2023-10-24 22:04:47 +0900

    [main] News/Ask/Jobs View Styling

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,10 +1,23 @@
 <template lang="">
-    <p v-for="item in this.$store.state.jobList" v-bind:key="item">
-        <a :href="item.url">
-            {{ item.title }}
-        </a>
-        <small>{{ item.time_ago }} {{ item.domain }}</small>
-    </p>
+    <div>
+        <ul class="news-list">
+            <li class="post" v-for="item in this.$store.state.jobList" v-bind:key="item">
+                <div class="points">
+                    {{ item.poinst || "0" }}
+                </div>
+                <div>
+                    <p class="news-title">
+                        <a :href="item.url">
+                            {{ item.title }}
+                        </a>
+                    </p>
+                    <small class="link-text">
+                        {{ item.time_ago }} by {{ item.domain }}
+                    </small>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -17,6 +30,49 @@ export default {
 
 </script>
 
-<style lang="">
-    
+<style scoped>
+
+a {
+    color: #34495e;
+    text-decoration: none;  /* 라우터링크의 텍스트 밑줄 효과제거 */
+}
+
+a:hover {
+    color: #42b883;
+    text-decoration: underline;
+}
+
+a.router-link-exact-active {
+    text-decoration: underline;
+}
+
+.news-list {
+    margin: 0;
+    padding: 0;
+}
+
+.post {
+    list-style: none;       /* li 태그 머리글 표시 제거 */
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #eee;
+}
+
+.points {
+    width: 80px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #42b883;
+}
+
+.news-title {
+    margin: 0px;
+}
+
+.link-text {
+    color: gray;
+}
+
 </style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -5,7 +5,6 @@
                 <div class="points">
                     {{ item.points }}
                 </div>
-
                 <div>
                     <p class="news-title">
                         <a v-bind:href="item.url" >
@@ -34,51 +33,47 @@ export default {
 
 <style>
 
-    body{
-        margin: 0;
-        padding: 0;
-    }
+a {
+    color: #34495e;
+    text-decoration: none;  /* 라우터링크의 텍스트 밑줄 효과제거 */
+}
 
-    a {
-        color: #34495e;
-        text-decoration: none;  /* 라우터링크의 텍스트 밑줄 효과제거 */
-    }
+a:hover {
+    color: #42b883;
+    text-decoration: underline;
+}
 
-    a:hover {
-        color: #42b883;
-        text-decoration: underline;
-    }
+a.router-link-exact-active {
+    text-decoration: underline;
+}
 
-    a.router-link-exact-active {
-        text-decoration: underline;
-    }
+.news-list {
+    margin: 0;
+    padding: 0;
+}
 
-    .news-list {
-        margin: 0;
-        padding: 0;
-    }
+.post {
+    list-style: none;       /* li 태그 머리글 표시 제거 */
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #eee;
+}
 
-    .post {
-        list-style: none;       /* li 태그 머리글 표시 제거 */
-        display: flex;
-        align-items: center;
-        border-bottom: 1px solid #eee;
-    }
+.points {
+    width: 80px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #42b883;
+}
 
-    .points {
-        width: 80px;
-        height: 60px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: #42b883;
-    }
+.news-title {
+    margin: 0px;
+}
 
-    .news-title {
-        margin: 0px;
-    }
+.link-text {
+    color: gray;
+}
 
-    .link-text {
-        color: gray;
-    }
 </style>
